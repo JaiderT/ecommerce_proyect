73ca49f2d488871f88b493b048c1025fafe7e4db
jaiderT <jaideralexandertotenabustamn@gmail.com>
2025-11-06 15:20:18 -0500

    actualizado

---
+++ backend/db/db.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+const uri ="mongodb+srv://adsotarde:adso2025@ecommer.4ccsxoe.mongodb.net/?appName=Tienda?retryWrites=true&w=majority";
+mongoose.connect(uri)
+.then( () => console.log("✅ Conectado a la base de datos"))
+.catch(err => console.log("❌ Error al conectar a la base de datos:", err));
