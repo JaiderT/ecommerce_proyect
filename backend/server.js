eb7e970c6a933a79419b716b51918b12c8269804
jaiderT <jaideralexandertotenabustamn@gmail.com>
2025-11-06 13:36:11 -0500

    actualizado

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+//habilitar todas las rutas
+
+app.use(cors());
+
+//primera ruta
+
+app.get('/', (req, res) => { 
+    res.send('Bienvenido al curso de node express');
+});
+
+app.listen(8081, () => console.log('Servidor corriendo en http://localhost:8081') )
