f653a71b827791c24e43a3245f6b751e1c064e93
jaiderT <jaideralexandertotenabustamn@gmail.com>
2025-12-05 17:10:05 -0500

    nuevo

---
+++ src/pages/java/newpass.js
@@ -0,0 +1,66 @@
+// Validar que se haya solicitado un código antes
+const email = localStorage.getItem("email");
+
+if (!email) {
+    // Si el usuario intenta abrir nuevapassword.html directamente → lo mandamos a recuperar
+    window.location.href = "./recuperar.html";
+}
+
+document.getElementById("newpass-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const codigo = document.getElementById("code").value.trim();
+    const nuevapassword = document.getElementById("new_pass").value.trim();
+    const confirmarPassword = document.getElementById("confirm_pass").value.trim();
+    const btn = document.getElementById('newpass-btn');
+
+    const codeError = document.getElementById("code-error");
+    const passError = document.getElementById("pass-error");
+    const confirmError = document.getElementById("confirm-error");
+
+    codeError.classList.add("hidden");
+    passError.classList.add("hidden");
+    confirmError.classList.add("hidden");
+
+    if (nuevapassword !== confirmarPassword) {
+        confirmError.textContent = "Las contraseñas no coinciden";
+        confirmError.classList.remove("hidden");
+        return;
+    }
+
+    btn.disabled = true;
+    btn.textContent = 'Actualizando Contraseña...';
+
+    try {
+        const response = await fetch("https://ecommerce-proyect-kul6.onrender.com/api/Recuperar/cambiar-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                codigo,
+                nuevapassword
+            }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            codeError.textContent = data.message || "Código incorrecto";
+            codeError.classList.remove("hidden");
+            return;
+        }
+
+        // Limpiar datos
+        localStorage.removeItem("email");
+
+        // Redirigir al login
+        window.location.href = "./login.html";
+
+    } catch (error) {
+        console.error("Error:", error);
+        codeError.textContent = "Error de servidor";
+        codeError.classList.remove("hidden");
+    }
+});
---
+++ src/pages/java/recuperar.js
@@ -0,0 +1,42 @@
+document.getElementById("recuperar-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const errorEmail = document.getElementById("email-error");
+    const btn = document.getElementById('recupera-btn');
+
+    // limpiar mensaje
+    errorEmail.classList.add("hidden");
+
+    btn.disabled = true;
+    btn.textContent = 'Enviando Codigo...';
+
+    try {
+        const response = await fetch("https://ecommerce-proyect-kul6.onrender.com/api/Recuperar/solicitar-codigo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            errorEmail.textContent = data.message || "Error en la solicitud";
+            errorEmail.classList.remove("hidden");
+            return;
+        }
+
+        // Guardamos el email para usarlo en nuevapassword
+        localStorage.setItem("email", email);
+
+        // REDIRIGIR A nueva contraseña
+        setTimeout(() => window.location.href = 'nueva_password.html', 2000);
+
+    } catch (error) {
+        console.error("Error:", error);
+        errorEmail.textContent = "Error de servidor";
+        errorEmail.classList.remove("hidden");
+    }
+});
